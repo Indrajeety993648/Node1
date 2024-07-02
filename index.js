52af5d2d44e44ea4cb27f21f0d34d7264c7b5667
indrajeetyadav <indrajeetyadav993648@gmail.com>
2024-07-02 23:12:05 +0530

    Port Chnages

--- index.js
+++ index.js
@@ -77,25 +77,26 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res) => {
-  const filePath = path.join(__dirname, 'index.html');
-
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      res.writeHead(500, {'Content-Type': 'text/plain'});
-      res.end('Internal Server Error');
-      console.error('Error reading file:', err.message);
-      return;
-    }
-
     res.setHeader('Content-Type', 'text/html');
-    res.writeHead(200);
-    res.end(data);
-  });
+    if (req.url === '/login') {
+        const filepath = path.join(__dirname, 'index.html');
+        try {
+            const content = fs.readFileSync(filepath, 'utf8');
+            res.write(content);
+        } catch (err) {
+            console.error(err);
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.write('Internal Server Error');
+        }
+    } else {
+        res.write('<html> <head> <title>My First Page</title> </head> <body> <h1>Hello World!</h1> </body> </html>');
+    }
+    res.end();
 });
 
-const port = 3002;
+const port = 3004;
 const host = 'localhost';
 
 server.listen(port, host, () => {
-  console.log(`Server is listening on http://${host}:${port}`);
+    console.log(`Server is running at http://${host}:${port}`);
 });
